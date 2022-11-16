4010e5ae1ee1ff1a49b52e815539519f8c1a12d4
Dheeraj4103 <dheerajjadhav4455@gmail.com>
2022-11-17 00:34:45 +0530

    Fibonacci with DP

--- 7_Recursion/src/Fibonacci.java
+++ 7_Recursion/src/Fibonacci.java
@@ -1,11 +1,31 @@
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(Fib(50));
+        long fibs[] = new long[51];
+        System.out.println(FibwithDP(50, fibs));
+        System.out.println(Arrays.toString(fibs));
     }
     static int Fib(int n){
         if (n < 2) {
             return n;
         }
         return Fib(n-1) + Fib(n-2);
+    }
+
+    static long FibwithDP(int n, long[] fibs){
+        if (n < 2) {
+            fibs[n] = n;
+            return (long)n;
+        }
+
+        if (fibs[n-1] == 0){
+            fibs[n-1] = FibwithDP(n-1, fibs);
+        }
+        if (fibs[n-2] == 0){
+            fibs[n-2] = FibwithDP(n-2, fibs);
+        }
+        fibs[n] = fibs[n-1] + fibs[n-2];
+        return fibs[n];
     }
 }
--- 7_Recursion/src/allsubstrings.java
+++ 7_Recursion/src/allsubstrings.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class allsubstrings {
     public static void main(String[] args) {
